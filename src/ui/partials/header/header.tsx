717eed3a444a30b672f1c582f00e6528fe4990fe
arielsardinha <odararepresentacoes@outlook.com.br>
2021-11-06 10:06:50 -0300

    tirando textos do header

--- src/ui/partials/header/header.tsx
+++ src/ui/partials/header/header.tsx
@@ -5,19 +5,18 @@ const Header = () => (
   <header id="home" className="header-container containe">
     <DataInformation />
     <div className="box-header">
-      <div className="box-titles" data-aos="fade-up">
+      {/* <div className="box-titles" data-aos="fade-up">
         <h1>Nós o ajudamos a realizar seus objetivos de negócios</h1>
         <p>
           desde 1890, quando a empresa foi fundada em germen por jeorge vital
           bin truckpress tornou-se um dos principais fornecedores de logística
           do mundo
         </p>
-        <div>
-          <a href="https://WA.me/556333221580" target="_blank" rel="noreferrer">
-            Adquira o serviço
-          </a>
-        </div>
-      </div>
+        <div></div>
+      </div> */}
+      {/* <a href="https://WA.me/556333221580" target="_blank" rel="noreferrer">
+        Adquira o serviço
+      </a> */}
     </div>
   </header>
 );
